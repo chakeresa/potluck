e8354bb4f4c1544c57c018b814edf922ece51b35
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-18 22:21:45 -0600

    Abstract menu_generator into MenuGenerator class from Potluck

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,4 +1,5 @@
 require './lib/dish'
+require './lib/menu_generator'
 
 class Potluck
   attr_reader :date,
@@ -13,7 +14,8 @@ class Potluck
 
   def add_dish(dish)
     @dishes << dish
-    menu_generator
+    menu_generator = MenuGenerator.new(self)
+    @menu = menu_generator.menu
     @dishes
   end
 
@@ -21,33 +23,6 @@ class Potluck
     @dishes.select do |dish|
       dish.category == category
     end
-  end
-
-  # move to a new class?
-  def menu_generator
-    @menu = Hash.new([])
-
-    # move to another method
-    @dishes.each do |dish|
-
-      key = add_s(dish.category)
-
-      if !@menu.has_key?(key)
-        @menu[key] = []
-      end
-
-      @menu[key] << dish.name
-    end
-
-    # move to another method
-    @menu.each_key do |category|
-      @menu[category].sort!
-    end
-
-  end
-
-  def add_s(symbol)
-    (symbol.to_s + "s").to_sym
   end
 
 end
